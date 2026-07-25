0af17172d37e2b2febddb9a22255e18ee14a82fe
Mepoify <t_yaseen@icloud.com>
2026-07-25 10:25:30 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,33 @@
 const http = require("http");
 const WebSocket = require("ws");
 
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end("MEPO Chat Server is running!");
+});
 
 const wss = new WebSocket.Server({ server });
 
-wss.on("connection", socket => {
+console.log("Starting chat server...");
 
+wss.on("connection", socket => {
     console.log("Someone connected!");
 
     socket.on("message", message => {
-
         wss.clients.forEach(client => {
-
             if (client.readyState === WebSocket.OPEN) {
                 client.send(message.toString());
             }
-
         });
-
     });
 
     socket.on("close", () => {
         console.log("Someone disconnected");
     });
-
 });
 
-server.listen(port, () => {
-    console.log("Chat server running on port", port);
+server.listen(PORT, () => {
+    console.log(`Chat server running on port ${PORT}`);
 });
